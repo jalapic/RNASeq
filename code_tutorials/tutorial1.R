7effa6b28c8cbaa4c2db348d50df3af6cdc54c7f
Curley <curley@utexas.edu>
2023-02-15 12:02:05 -0600

    update tutorial

--- code_tutorials/tutorial1.R
+++ code_tutorials/tutorial1.R
@@ -130,4 +130,249 @@ x
 
 ##### Data Pre-Processing
 
+#' Transformations from the raw-scale
+#' 
+#' 
+#' For differential expression and related analyses, gene expression is rarely considered 
+#' at the level of raw counts since libraries sequenced at a greater depth will result in higher counts. 
+#' Rather, it is common practice to transform raw counts onto a scale that accounts for such library size 
+#' differences. Popular transformations include counts per million (CPM), log2-counts per million (log-CPM), 
+#' reads per kilobase of transcript per million (RPKM), and fragments per kilobase of transcript per
+#'  million (FPKM).
+#'  
+#'  In our analyses, CPM and log-CPM transformations are used regularly although 
+#'  they do not account for gene length differences as RPKM and FPKM values do. Whilst RPKM and FPKM 
+#'  values can just as well be used, CPM and log-CPM values can be calculated using 
+#'  a counts matrix alone and will suffice for the type of comparisons we are interested in. 
+#'  
+#'  Assuming that there are no differences in isoform usage between conditions, differential expression analyses 
+#'  look at gene expression changes between conditions rather than comparing expression across 
+#'  multiple genes or drawing conclusions on absolute levels of expression. In other words, 
+#'  gene lengths remain constant for comparisons of interest and any observed differences are
+#'   a result of changes in condition rather than changes in gene length.
+
+#' Here raw counts are converted to CPM and log-CPM values using the cpm function in edgeR. 
+#' RPKM values are just as easily calculated as CPM values using the rpkm function in edgeR 
+#' if gene lengths are available.
+
+cpm <- cpm(x)
+lcpm <- cpm(x, log=TRUE)
+
+
+#' A CPM value of 1 for a gene equates to having 20 counts in the sample with the lowest sequencing depth
+#'  (JMS0-P8c, library size approx. 20 million) or 76 counts in the sample with the greatest sequencing depth 
+#'  (JMS8-3, library size approx. 76 million).
+
+#' The log-CPM values will be used for exploratory plots. When log=TRUE, the cpm function adds an offset 
+#' to the CPM values before converting to the log2-scale. By default, the offset is 2/L where 2 is the “prior count” and L 
+#' is the average library size in millions, so the log-CPM values are related to the CPM values by log2(CPM + 2/L).
+#'  This calculation ensures that any two read counts with identical CPM values will also have identical log-CPM values. 
+#'  The prior count avoids taking the logarithm of zero, and also reduces spurious variability for genes with very low 
+#'  counts by shrinking all the inter-sample log-fold-changes towards zero, something that is helpful for exploratory plotting. 
+#'  For this dataset, the average library size is about 45.5 million, so L approx. 45.5 and the minimum log-CPM value for
+#'   each sample becomes log2(2/45.5) = -4.51. In other words, a counr of zero for this data maps to a log-CPM value of -4.51 
+#'   after adding the prior count or offset:
+
+
+L <- mean(x$samples$lib.size) * 1e-6
+M <- median(x$samples$lib.size) * 1e-6
+c(L, M)
+
+summary(lcpm)
+
+#' Log-CPM values are also used in downstream linear modeling via limma’s voom function, although voom recomputes 
+#' its own log-CPM values internally with a smaller prior count. 
+
+
+
+### Removing genes that are lowly expressed
+
+#' All datasets will include a mix of genes that are expressed and those that are not expressed. 
+#' Whilst it is of interest to examine genes that are expressed in one condition but not in another,
+#'  some genes are unexpressed throughout all samples. In fact, 19% of genes in this dataset have zero 
+#'  counts across all nine samples.
+
+
+table(rowSums(x$counts==0)==9)
+
+
+#' Plotting the distribution log-CPM values shows that a sizeable proportion of genes within each sample 
+#' are either unexpressed or lowly-expressed with log-CPM values that are small or negative (Figure 1A).
+
+#' Genes that do not have a worthwhile number of reads in any sample should be filtered out of the downstream analyses.
+#'  There are several reasons for this. From a biological point of view, genes that not expressed at a
+#'   biologically meaningful level in any condition are not of interest and are therefore best ignored. 
+#'   From a statistical point of view, removing low count genes allows the mean-variance relationship in the data
+#'    to be estimated with greater reliability and also reduces the number of statistical tests that need to
+#'     be carried out in downstream analyses looking at differential expression.
+
+
+#' The filterByExpr function in the edgeR package provides an automatic way to filter genes, 
+#' while keeping as many genes as possible with worthwhile counts.
+
+dim(x) #[1] 27179     9
+
+keep.exprs <- filterByExpr(x, group=group)
+x <- x[keep.exprs,, keep.lib.sizes=FALSE]
+dim(x)
+
+#' By default, the function keeps genes with about 10 read counts or more in a minimum number of samples, 
+#' where the number of samples is chosen according to the minimum group sample size. 
+#' The actual filtering uses CPM values rather than counts in order to avoid giving preference to samples with 
+#' large library sizes. For this dataset, the median library size is about 51 million and 10/51 approx. 0.2, so
+#'  the filterByExpr function keeps genes that have a CPM of 0.2 or more in at least three samples. 
+#'  A biologically interesting gene should be expressed in at least three samples because all the cell type 
+#'  groups have three replicates. The cutoffs used depend on the sequencing depth and on the experimental design. 
+#'  If the library sizes had been larger then a lower CPM cutoff would have been chosen, because larger library sizes 
+#'  provide better resolution to explore more genes at lower expression levels. Alternatively, smaller library sizes
+#'   decrease our ability to explore marginal genes and hence would have led to a higher CPM cutoff.
+
+#' Using this criterion, the number of genes is reduced to 16,624, about 60% of the number that we started with 
+#' (panel B of the next figure). Note that subsetting the entire DGEList-object removes both the counts and 
+#' the associated gene information for the filtered genes. The filtered DGEList-object keeps the gene 
+#' information and the counts for the retained genes correctly associated.
+
+
+
+## Make Plot of Distributions
+
+lcpm.cutoff <- log2(10/M + 2/L)
+library(RColorBrewer)
+nsamples <- ncol(x)
+col <- brewer.pal(nsamples, "Paired")
+par(mfrow=c(1,2))
+plot(density(lcpm[,1]), col=col[1], lwd=2, ylim=c(0,0.26), las=2, main="", xlab="")
+title(main="A. Raw data", xlab="Log-cpm")
+abline(v=lcpm.cutoff, lty=3)
+for (i in 2:nsamples){
+  den <- density(lcpm[,i])
+  lines(den$x, den$y, col=col[i], lwd=2)
+}
+legend("topright", samplenames, text.col=col, bty="n")
+lcpm <- cpm(x, log=TRUE)
+plot(density(lcpm[,1]), col=col[1], lwd=2, ylim=c(0,0.26), las=2, main="", xlab="")
+title(main="B. Filtered data", xlab="Log-cpm")
+abline(v=lcpm.cutoff, lty=3)
+for (i in 2:nsamples){
+  den <- density(lcpm[,i])
+  lines(den$x, den$y, col=col[i], lwd=2)
+}
+legend("topright", samplenames, text.col=col, bty="n")
+
+
+
+
+### Normalising gene expression distributions
+
+#' During the sample preparation or sequencing process, external factors that are not of biological interest
+#'  can affect the expression of individual samples. For example, samples processed in the first batch of an 
+#'  experiment can have higher expression overall when compared to samples processed in a second batch. It is 
+#'  assumed that all samples should have a similar range and distribution of expression values. Normalisation 
+#'  is required to ensure that the expression distributions of each sample are similar across the entire experiment.
+
+#' Any plot showing the per sample expression distributions, such as a density or boxplot, is useful in determining
+#'  whether any samples are dissimilar to others. Distributions of log-CPM values are similar throughout all
+#'   samples within this dataset (panel B of the figure above).
+
+#' Nonetheless, normalisation by the method of trimmed mean of M-values (TMM) (Robinson and Oshlack 2010) is 
+#' performed using the calcNormFactors function in edgeR. The normalisation factors calculated here are used 
+#' as a scaling factor for the library sizes. When working with DGEList-objects, these normalisation factors 
+#' are automatically stored in x$samples$norm.factors. For this dataset the effect of TMM-normalisation is mild,
+#'  as evident in the magnitude of the scaling factors, which are all relatively close to 1.
+
+
+x <- calcNormFactors(x, method = "TMM")
+x$samples$norm.factors
+
+#' To give a better visual representation of the effects of normalisation, the data was duplicated then
+#'  adjusted so that the counts of the first sample are reduced to 5% of 
+#' their original values, and in the second sample they are inflated to be 5-times larger.
+#' 
+#' The figure below shows the expression distribution of samples for unnormalised and normalised data,
+#'  where distributions are noticeably different pre-normalisation and are similar post-normalisation. 
+#'  Here the first sample has a small TMM scaling factor of 0.06, whereas the second sample has a large 
+#'  scaling factor of 6.08 – neither values are close to 1.
+
+
+
+x2 <- x
+x2$samples$norm.factors <- 1
+x2$counts[,1] <- ceiling(x2$counts[,1]*0.05)
+x2$counts[,2] <- x2$counts[,2]*5
+
+par(mfrow=c(1,2))
+lcpm <- cpm(x2, log=TRUE)
+boxplot(lcpm, las=2, col=col, main="")
+title(main="A. Example: Unnormalised data",ylab="Log-cpm")
+x2 <- calcNormFactors(x2)  
+x2$samples$norm.factors
+
+
+lcpm <- cpm(x2, log=TRUE)
+boxplot(lcpm, las=2, col=col, main="")
+title(main="B. Example: Normalised data",ylab="Log-cpm")
+
+
+
+### Unsupervised clustering of samples
+
+#' In our opinion, one of the most important exploratory plots to examine for gene expression analyses
+#'  is the multi-dimensional scaling (MDS) plot, or similar. The plot shows similarities and dissimilarities
+#'   between samples in an unsupervised manner so that one can have an idea of the extent to which 
+#'   differential expression can be detected before carrying out formal tests. Ideally, samples would cluster
+#'    well within the primary condition of interest, and any sample straying far from its group could be 
+#'    identified and followed up for sources of error or extra variation. If present, technical replicates 
+#'    should lie very close to one another.
+
+#' Such a plot can be made in limma using the plotMDS function. The first dimension represents the leading-fold-change
+#'  that best separates samples and explains the largest proportion of variation in the data, with subsequent 
+#'  dimensions having a smaller effect and being orthogonal to the ones before it. When experimental design 
+#'  involves multiple factors, it is recommended that each factor is examined over several dimensions. If samples cluster by
+#'   a given factor in any of these dimensions, it suggests that the factor contributes to expression differences and is worth
+#'    including in the linear modelling. On the other hand, factors that show little or no effect may be left
+#'     out of downstream analysis.
+
+
+#' In this dataset, samples can be seen to cluster well within experimental groups over dimension 1 and 2, 
+#' and then separate by sequencing lane (sample batch) over dimension 3 (shown in the plot below).
+#'  Keeping in mind that the first dimension explains the largest proportion of variation in the
+#'   data, notice that the range of values over the dimensions become smaller as we move to higher dimensions.
+
+#' Whilst all samples cluster by groups, the largest transcriptional difference is observed between basal 
+#' and LP, and basal and ML over dimension 1. For this reason, it is expected that pairwise comparisons between
+#'  cell populations will result in a greater number of DE genes for comparisons involving basal samples, and 
+#'  relatively small numbers of DE genes when comparing ML to LP. Datasets where samples do not cluster by
+#'   experimental group may show little or no evidence of differential expression in the downstream analysis.
+
+#' To create the MDS plots, we assign different colours to the factors of interest. 
+#' Dimensions 1 and 2 are examined using the color grouping defined by cell types.
+
+#' Dimensions 3 and 4 are examined using the colour grouping defined by sequencing lanes (batch).
+
+lcpm <- cpm(x, log=TRUE)
+par(mfrow=c(1,2))
+col.group <- group
+levels(col.group) <-  brewer.pal(nlevels(col.group), "Set1")
+col.group <- as.character(col.group)
+col.lane <- lane
+levels(col.lane) <-  brewer.pal(nlevels(col.lane), "Set2")
+col.lane <- as.character(col.lane)
+plotMDS(lcpm, labels=group, col=col.group)
+title(main="A. Sample groups")
+plotMDS(lcpm, labels=lane, col=col.lane, dim=c(3,4))
+title(main="B. Sequencing lanes")
+
+
+#' Alternatively, the Glimma package offers the convenience of an interactive MDS plot where multiple 
+#' dimensions can be explored. The glMDSPlot function generates an html page (that is opened in a browser 
+#' if launch=TRUE) with an MDS plot in the left panel and a barplot showing the proportion of variation explained
+#'  by each dimension in the right panel. Clicking on the bars of the bar plot changes the pair of dimensions plotted
+#'   in the MDS plot, and hovering over the individual points reveals the sample label. The colour scheme can 
+#'   be changed as well to highlight cell population or sequencing lane (batch). An interactive MDS plot of 
+#'   this dataset can be found at http://bioinf.wehi.edu.au/folders/limmaWorkflow/glimma-plots/MDS-Plot.html.
+
+
+glMDSPlot(lcpm, labels=paste(group, lane, sep="_"), 
+          groups=x$samples[,c(2,5)], launch=TRUE)
+
 
